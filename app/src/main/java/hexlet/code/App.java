8567b496a7d0ae2529c74047e0f312621d6ef799
Artur Lastovskiy <4372wow@gmail.com>
2024-02-02 20:01:41 +0300

    update project dependencies and src

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class App {
+
+    private int port;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World");
+
+        var app = getApp();
+        app.start();
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(javalinConfig -> {
+            javalinConfig.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World!"));
+
+        return app;
     }
 }
