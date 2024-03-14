b796a5a54300fdfafde2834b37dcf62af73fd219
Artur Lastovskiy <4372wow@gmail.com>
2024-03-14 16:32:19 +0300

    update project

--- app/src/main/java/hexlet/code/dto/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/UrlPage.java
@@ -2,6 +2,9 @@ package hexlet.code.dto;
 
 import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.Getter;
 
@@ -15,6 +18,12 @@ public class UrlPage extends MainPage {
         super(message);
         this.url = url;
         this.checks = checks;
+    }
+
+    public final List<UrlCheck> reverseList(List<UrlCheck> checks) {
+                var copyChecks = new ArrayList<>(checks);
+                Collections.reverse(copyChecks);
+                return copyChecks;
     }
 
 }
